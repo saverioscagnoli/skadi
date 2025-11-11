61acd0d1731bb32272a713cf61dc633519a23655
Saverio Scagnoli <95577971+saverioscagnoli@users.noreply.github.com>
2025-11-11 12:57:49 +0100

    info parameters

--- wwwatch/src/main.rs
+++ wwwatch/src/main.rs
@@ -2,6 +2,7 @@ mod payloads;
 
 use crate::payloads::WorkspacePayload;
 use clap::Parser;
+use indexmap::IndexSet;
 use serde::Serialize;
 use std::error::Error;
 use swayipc::{Connection, Event, EventType, WorkspaceChange};
@@ -20,6 +21,42 @@ struct Args {
         default_value_t = false
     )]
     workspaces: bool,
+    #[arg(
+        short,
+        long,
+        help = "Queries cpu info every <interval> milliseconds",
+        default_value_t = false
+    )]
+    cpu: bool,
+    #[arg(
+        short,
+        long,
+        help = "Queries memory info every <interval> milliseconds",
+        default_value_t = false
+    )]
+    mem: bool,
+
+    #[arg(
+        short,
+        long,
+        help = "Queries disk info every <interval> milliseconds",
+        default_value_t = false
+    )]
+    disk: bool,
+    #[arg(
+        short,
+        long,
+        help = "Queries network info every <interval> milliseconds",
+        default_value_t = false
+    )]
+    network: bool,
+    #[arg(
+        short,
+        long,
+        help = "Interval in milliseconds, used to query information about the system",
+        default_value_t = 1000
+    )]
+    interval: u64,
 }
 
 #[derive(Debug, Clone, Copy, Serialize)]
@@ -31,48 +68,75 @@ enum Op {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let mut events_list = Vec::new();
-    let mut event_connection = Connection::new()?;
+    let mut event_list = Vec::new();
+    let mut connection = Connection::new()?;
+    let mut current_workspace = String::new();
+    let mut workspace_cache = IndexSet::new();
 
     if args.workspaces {
-        events_list.push(EventType::Workspace);
+        event_list.push(EventType::Workspace);
+        // Initial check to workspaces
 
-        let payload = WorkspacePayload::new(&mut event_connection);
-        println!("{:?}", serde_json::to_string(&payload)?);
+        let workspaces = connection.get_workspaces()?;
+
+        for ws in workspaces {
+            if ws.focused {
+                current_workspace = ws.name.clone();
+            }
+            workspace_cache.insert(ws.name);
+        }
+
+        let payload = WorkspacePayload {
+            op: Op::Workspaces,
+            current: &current_workspace,
+            total: &workspace_cache,
+        };
+
+        println!("{}", serde_json::to_string(&payload).unwrap());
     }
 
     // One connection for events
-    let events = event_connection.subscribe(&events_list)?;
-
-    // Separate connection for queries
-    let mut query_connection = Connection::new()?;
+    let events = connection.subscribe(&event_list)?;
 
     // Listen for events
     for event in events.map_while(Result::ok) {
         match event {
-            Event::Workspace(ws_event) => {
-                if matches!(ws_event.change, WorkspaceChange::Focus) {
-                    let mut payload = WorkspacePayload {
-                        op: Op::Workspaces,
-                        current: 0,
-                        total: Vec::new(),
-                    };
-
-                    if let Some(current) = ws_event.current {
-                        payload.current =
-                            current.name.and_then(|n| n.parse::<u8>().ok()).unwrap_or(0);
+            Event::Workspace(ws_event) => match ws_event.change {
+                WorkspaceChange::Focus => {
+                    if let Some(current_ws) = ws_event.current
+                        && let Some(name) = current_ws.name
+                    {
+                        workspace_cache.insert(name.clone());
+                        current_workspace = name;
                     }
 
-                    // Use the dedicated query connection
-                    let workspaces = query_connection.get_workspaces()?;
-                    payload.total = workspaces
-                        .iter()
-                        .filter_map(|ws| ws.name.parse::<u8>().ok())
-                        .collect();
+                    let payload = WorkspacePayload {
+                        op: Op::Workspaces,
+                        current: &current_workspace,
+                        total: &workspace_cache,
+                    };
 
-                    println!("{:?}", serde_json::to_string(&payload)?);
+                    println!("{}", serde_json::to_string(&payload)?);
                 }
-            }
+
+                WorkspaceChange::Empty => {
+                    if let Some(current_ws) = ws_event.current
+                        && let Some(name) = current_ws.name
+                    {
+                        workspace_cache.swap_remove(&name);
+                    }
+
+                    let payload = WorkspacePayload {
+                        op: Op::Workspaces,
+                        current: &current_workspace,
+                        total: &workspace_cache,
+                    };
+
+                    println!("{}", serde_json::to_string(&payload).unwrap());
+                }
+
+                _ => {}
+            },
             _ => {}
         }
     }
--- wwwatch/src/payloads.rs
+++ wwwatch/src/payloads.rs
@@ -1,38 +1,10 @@
-use serde::Serialize;
-use swayipc::Connection;
-
 use crate::Op;
+use indexmap::IndexSet;
+use serde::Serialize;
 
 #[derive(Debug, Clone, Serialize)]
-pub struct WorkspacePayload {
+pub struct WorkspacePayload<'a> {
     pub op: Op,
-    pub current: u8,
-    pub total: Vec<u8>,
-}
-
-impl WorkspacePayload {
-    pub fn new(conn: &mut Connection) -> Self {
-        let mut s = Self {
-            op: Op::Workspaces,
-            current: 0,
-            total: Vec::new(),
-        };
-
-        let workspaces = conn.get_workspaces().expect("Failed to get workspaces");
-
-        for w in &workspaces {
-            let name = w
-                .name
-                .parse::<u8>()
-                .expect("Workspace name is not a number");
-
-            if w.focused {
-                s.current = name;
-            }
-
-            s.total.push(name);
-        }
-
-        s
-    }
+    pub current: &'a String,
+    pub total: &'a IndexSet<String>,
 }
